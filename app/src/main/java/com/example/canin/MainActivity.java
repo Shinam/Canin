7641e19886f66fdd20cc9a7a2ea7aaee5f8a0136
Shinam <arnaud.verkenne@gmail.com>
2017-01-28 18:56:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/canin/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.canin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation fade_in, fade_out;
+    ViewFlipper viewFlipper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.flip);
+        fade_in = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in);
+        fade_out = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out);
+        viewFlipper.setInAnimation(fade_in);
+        viewFlipper.setOutAnimation(fade_out);
+//sets auto flipping
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.startFlipping();
+    }
+}
