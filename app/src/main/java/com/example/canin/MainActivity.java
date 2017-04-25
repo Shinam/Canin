0b21355ee5307fd9c4cb84271a39a6eda00cf436
Shinam <arnaud.verkenne@gmail.com>
2017-04-25 15:36:51 +0200

    Image Glide

--- app/src/main/java/com/example/canin/MainActivity.java
+++ app/src/main/java/com/example/canin/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.canin;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView img;
+        img = (ImageView) findViewById(R.id.pict0);
+        Glide.with(this).load(R.drawable.pict0).into(img);
+        img = (ImageView) findViewById(R.id.pict1);
+        Glide.with(this).load(R.drawable.pict1).into(img);
+        img = (ImageView) findViewById(R.id.pict2);
+        Glide.with(this).load(R.drawable.pict2).into(img);
+        img = (ImageView) findViewById(R.id.pict3);
+        Glide.with(this).load(R.drawable.pict3).into(img);
+        img = (ImageView) findViewById(R.id.pict4);
+        Glide.with(this).load(R.drawable.pict4).into(img);
+        img = (ImageView) findViewById(R.id.pict5);
+        Glide.with(this).load(R.drawable.pict5).into(img);
+        img = (ImageView) findViewById(R.id.pict6);
+        Glide.with(this).load(R.drawable.pict6).into(img);
+        img = (ImageView) findViewById(R.id.pict7);
+        Glide.with(this).load(R.drawable.pict7).into(img);
+
         viewFlipper = (ViewFlipper) this.findViewById(R.id.flip);
         fade_in = AnimationUtils.loadAnimation(this,
                 android.R.anim.fade_in);
